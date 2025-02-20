eb7a73bb096e61882b1c9f3d8893e9b7eae79b4e
rinz24 <147013177+rinz24@users.noreply.github.com>
2025-02-20 17:26:55 +0700

    Add files via upload

---
+++ week3.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".container");
+    if (container) {
+        container.style.transform = "scale(1.1)";
+        container.style.transformOrigin = "center";
+    }
+});
